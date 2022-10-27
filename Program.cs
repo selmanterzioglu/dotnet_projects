e8ce76f982cc4c95da3cd0a446706d4804d8cebe
selmantercioglu <selmantercioglu@gmail.com>
2022-10-27 13:47:58 +0300

    added first code

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Template {
+    internal  class Template {
+        static void Main(String[] args)
+        {
+                
+        }
+        static double Avarage (int[] x) 
+        {
+            double res = 0;
+            for (int i = 0; i < x.Length; i++){res += x[i];}
+            return res / x.Length;
+        }
+    }
+}
