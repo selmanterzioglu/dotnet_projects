de15e0cd90ec527130336a69e4202c9fae22f177
selmantercioglu <selmantercioglu@gmail.com>
2022-10-31 16:27:12 +0300

    addded programç.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Program {
     internal  class Program {
         static void Main(String[] args)
         {
+            int[] a = new int []{1, 8, 2, 9};
                 
         }
         static double Avarage (int[] x) 
@@ -16,6 +16,17 @@ namespace Program {
             double res = 0;
             for (int i = 0; i < x.Length; i++){res += x[i];}
             return res / x.Length;
+        }
+
+        static int Max(int[] x)
+        {
+            int max = x[0];
+            for (int i = 1; i < x.Length; i++)
+            {
+                if(x[i] > max)
+                    max = x[i];
+            }
+            return max; 
         }
     }
 }
